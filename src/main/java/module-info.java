47f384d6a87f0a4fcaccf3d17bf1103bc89a1eeb
anderc1-debug <anderc1@farmingdale.edu>
2026-05-03 22:45:09 -0400

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.nightowl {
     requires javafx.fxml;
     requires javafx.web;
     requires java.desktop;
+    requires java.sql;
+    requires derby;
 
     opens com.nightowl to javafx.fxml;
     exports com.nightowl;
