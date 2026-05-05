13ede3cac4a0098fccd60f48abc937cd2d03dde2
anderc1-debug <anderc1@farmingdale.edu>
2026-05-05 01:05:41 -0400

    Update Main.java

--- src/main/java/com/nightowl/Main.java
+++ src/main/java/com/nightowl/Main.java
@@ -4,8 +4,4 @@ public class Main {
     public static void main(String[] args) {
         MainApp.main(args);
     }
-    @Override
-public void stop() {
-    DatabaseManager.getInstance().shutdown();
-}
 }
